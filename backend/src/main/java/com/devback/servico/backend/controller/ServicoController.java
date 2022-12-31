8f7028153745949471bcba3d8f3d4261dc406876
Elton Lima <96541770+DevBack@users.noreply.github.com>
2022-12-31 18:39:55 -0300

    Create Service Controller

---
+++ backend/src/main/java/com/devback/servico/backend/controller/ServicoController.java
@@ -0,0 +1,57 @@
+package com.devback.servico.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devback.servico.backend.entity.Servico;
+import com.devback.servico.backend.service.ServicoService;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/servicos")
+public class ServicoController {
+
+	@Autowired
+	private ServicoService servicoService;
+	
+	@GetMapping("/")
+	public List<Servico> getAll() {
+		return servicoService.findAll();
+	}
+	
+	@GetMapping("/pendentes")
+	public List<Servico> getPendentes() {
+		return servicoService.findByStatusPendente();
+	}
+	
+	@GetMapping("/cancelados")
+	public List<Servico> getCancelados() {
+		return servicoService.findByStatusCancelado();
+	}
+	
+	@PostMapping("/")
+	public Servico insert(@RequestBody Servico servico) {
+		return servicoService.insert(servico);
+	}
+	
+	@PutMapping("/")
+	public Servico update(@RequestBody Servico servico) {
+		return servicoService.update(servico);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathParam("id") Long id) {
+		servicoService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+}
