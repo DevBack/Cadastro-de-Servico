f0aeddeef7971bbbd0cde84b12131a2ac1abde55
DevBack <heltonlima.ti@live.com>
2022-12-31 15:29:59 -0300

    Update Servico Repository

--- backend/src/main/java/com/devback/servico/backend/repository/ServicoRepository.java
+++ backend/src/main/java/com/devback/servico/backend/repository/ServicoRepository.java
@@ -1,10 +1,17 @@
 package com.devback.servico.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devback.servico.backend.entity.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
 
+	@Query("select servicos from Servico servico where servico.valorPago <> null and servico.valorPago > 0")
+	List<Servico> findByStatusPendente();
 	
+	@Query("select servicos from Servico servico where servico.status = 'Cancelado'")
+	List<Servico> findByStatusCancelado();
 }
